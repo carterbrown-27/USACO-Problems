fda3e604493d9a0db363551a75ea0da9118944fd
carterbrown-27 <carterbrown@cmail.carleton.ca>
2020-01-11 18:48:40 -0500

    Home on the Range Solution

---
+++ src/range.java
@@ -0,0 +1,76 @@
+/*
+ID: carter.2
+LANG: JAVA
+TASK: range
+ */
+import java.io.*;
+import java.util.*;
+public class range {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		long first_time = System.nanoTime();
+
+		BufferedReader in;
+		String file = "range.in";
+		try{
+			in = new BufferedReader(new FileReader(file));
+		}catch(Exception e) {
+			in = new BufferedReader(new FileReader("DATA/"+file));
+		}
+
+		// 2d array
+		// for each spot (= top left corner)
+		// find squares.
+		int N = Integer.parseInt(in.readLine());
+		int[][] field = new int[N][N];
+
+		for(int y = 0; y < N; y++) {
+			String[] str = in.readLine().split("");
+			for(int x = 0; x < N; x++) {
+				field[x][y] = Integer.parseInt(str[x]);
+			}
+		}
+
+		// 0 ---> N
+		int[] tally = new int[N+1];
+		
+		search:
+		for(int y = 0; y < N; y++) {
+			for(int x = 0; x < N; x++) {
+				squareCheck: 
+				for(int s = 0; x+s < N && y+s < N; s++) {
+					// check next lines of square
+					for(int dx = 0; dx <= s; dx++) {
+						if(field[x+dx][y+s] == 0) break squareCheck;
+					}
+
+					for(int dy = 0; dy <= s; dy++) {
+						if(field[x+s][y+dy] == 0) break squareCheck;
+					}
+
+					// valid
+					tally[s]++;
+					
+					// if this = size of field (worst-case otherwise).
+					if(s==N-1) {
+						for(int i = s-1; i >= 1; i--) {
+							tally[i] = ((s-i)+1) * ((s-i)+1); // (s-i+1)^2.
+						}
+						break search;
+					}
+				}
+			}
+		}
+		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("range.out")));
+		for(int i = 1; i < tally.length; i++) {
+			if(tally[i] > 0) {
+				// System.out.printf("%d %d\n", i+1, tally[i]);
+				out.printf("%d %d\n", i+1, tally[i]);
+			}
+		}
+		out.close();
+		System.out.println((System.nanoTime() - first_time) / 1000000 + "ms [O]");
+	}
+}
--- src/shopping2.java
+++ src/shopping2.java
@@ -5,7 +5,7 @@ TASK: shopping
  */
 import java.io.*;
 import java.util.*;
-public class shopping {
+public class shopping2 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -57,10 +57,61 @@ public class shopping {
 			offers.put(q_map,Math.min(offers.getOrDefault(q_map,Integer.MAX_VALUE),price));
 		}
 		
-		int[][][][][] solutions = new int[5][5][5][5][5];
+		// this only works as a fractional approach, meaning it is not correct.
+		// TODO: instead, use Integer Knapsack Algorithm.
+		ArrayList<HashMap<Integer,Integer>> orderedOffers = new ArrayList<>(offers.keySet());
+		Collections.sort(orderedOffers, (Comparator<? super HashMap<Integer,Integer>>) (HashMap<Integer,Integer> a, HashMap<Integer,Integer> b) -> {
+			Double aE;
+			Double bE;
+			
+			int aOriginalP = 0;
+			Integer aTQ = 0;
+			for(Integer i: a.keySet()) {
+				aOriginalP += a.get(i) * prices.get(i);
+				aTQ += a.get(i);
+			}
+			
+			int bOriginalP = 0;
+			Integer bTQ = 0;
+			for(Integer i: b.keySet()) {
+				bOriginalP += b.get(i) * prices.get(i);
+				bTQ += b.get(i);
+			}
+			
+			aE = aOriginalP/ (double) offers.get(a);
+			bE = bOriginalP/ (double) offers.get(b);
+			
+			return aE.compareTo(bE) != 0 ? -aE.compareTo(bE) : -aTQ.compareTo(bTQ);
+		});
 		
-		// solution = # a-e.
+		// DEBUG:
+		System.out.println(orderedOffers.size());
+		orderedOffers.forEach((i) -> {
+			i.forEach((k,v) -> System.out.print(k+" "+v+","));
+			System.out.println();
+		});
+		
 		int price = 0;
+		for(HashMap<Integer,Integer> offer: orderedOffers) {
+			int amt = Integer.MAX_VALUE;
+			for(Integer item: offer.keySet()) {
+				// if item is excessive
+				if(needed.get(item) < offer.get(item)) {
+					amt = 0;
+					break;
+				} else {
+					// int division gives desired result;
+					amt = Math.min(amt, needed.get(item) / offer.get(item));
+				}
+			}
+			
+			price += amt * offers.get(offer);
+			for(Integer item: offer.keySet()) {
+				// remove q*amt from needed.
+				needed.put(item, needed.get(item) - (amt*offer.get(item)));
+			}
+		}
+		
 		System.out.println(price);
 		
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("shopping.out")));
